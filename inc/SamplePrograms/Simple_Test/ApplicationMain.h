24c718670b04c09da5af7b543769edf47e467666
Alek Krupka <krupka@rpi.edu>
2024-12-13 15:15:04 -0500

    Some stuff changed

---
+++ inc/SamplePrograms/Simple_Test/ApplicationMain.h
@@ -0,0 +1,16 @@
+/*
+ * program.h
+ *
+ *  Created on: Dec 13, 2024
+ *      Author: Alek
+ */
+
+#ifndef INC_SAMPLEPROGRAMS_SIMPLE_TEST_APPLICATIONMAIN_H_
+#define INC_SAMPLEPROGRAMS_SIMPLE_TEST_APPLICATIONMAIN_H_
+
+#include "init.h"
+
+int applicationMain();
+
+
+#endif /* INC_SAMPLEPROGRAMS_SIMPLE_TEST_APPLICATIONMAIN_H_ */
---
+++ src/SamplePrograms/Simple_Test/ApplicationMain.c
@@ -0,0 +1,18 @@
+/*
+ * ApplicationMain.c
+ *
+ *  Created on: Dec 13, 2024
+ *      Author: Alek
+ */
+
+#include "SamplePrograms/Simple_Test/ApplicationMain.h"
+
+int applicationMain()
+{
+	uint16_t a = 0;
+
+	while (1)
+	{
+		a++;
+	}
+}
