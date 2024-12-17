1346e1552386e73b1d171495484f7ecdc3601a76
Alek Krupka <krupka@rpi.edu>
2024-12-17 11:40:29 -0500

    Added some code for the cartridge

---
+++ src/cartridge_object.c
@@ -0,0 +1,18 @@
+/*
+ * cartridge_object.c
+ *
+ *  Created on: Dec 17, 2024
+ *      Author: Alek
+ */
+
+#include "cartridge_object.h"
+
+bool ensure_device_id_is_still_the_same(cartridge_t const * const cart, uint8_t const cart_slot_ind)
+{
+
+}
+
+bool init_flash_cartridge(cartridge_t * const cart, uint8_t cart_slot_ind)
+{
+
+}
